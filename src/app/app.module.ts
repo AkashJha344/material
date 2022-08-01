3828635064f023048bb34805c21c06b6aca672a6
Krantitech <akash,jha@krantiservices.in>
2022-08-01 10:47:27 +0530

    material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +17,6 @@ import { AuthInterceptorInterceptor } from './material/interceptor/auth-intercep
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    CommonModule
 
   ],
 
--- src/app/material/users/users.module.ts
+++ src/app/material/users/users.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+// import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { LoginComponent } from './login/login.component';
 import { SingupComponent } from './singup/singup.component';
 import { UserstableComponent } from './userstable/userstable.component';
 import { MatModule } from './mat/mat.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -16,9 +17,11 @@ import { MatModule } from './mat/mat.module';
     UserstableComponent
   ],
   imports: [
-    CommonModule,
+    // CommonModule,
+    BrowserModule,
     UsersRoutingModule,
     MatModule,
+    ReactiveFormsModule
     
   ]
 })
